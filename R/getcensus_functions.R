11370a2777a5fa3441698979f3350f10d9b3b7e3
Hannah Recht <hrecht@users.noreply.github.com>
2017-04-21 19:42:36 -0400

    fix misplaced chars causing build failure

--- R/getcensus_functions.R
+++ R/getcensus_functions.R
@@ -3,7 +3,7 @@
 #' @param apiurl, key, get, region, time
 #' @keywords internal
 #' @export
-#' @examples none
+#' @examples
 #' getFunction()
 getFunction <- function(apiurl, key, get, region, regionin, time, date, period, monthly, category_code, data_type_code) {
 	# Return API's built in error message if invalid call
