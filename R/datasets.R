225920df666e4d6dd2f78cf513ec68a48645abdd
Hannah Recht <hrecht@users.noreply.github.com>
2019-01-06 16:23:52 -0500

    pad fips with 0s to close #48

--- R/datasets.R
+++ R/datasets.R
@@ -2,7 +2,7 @@
 #'
 #' Some small geographies in some Census APIs can only be used under a state hierarchy.
 #' This is a list of fips codes that may be looped over to retrieve data for all states.
-#' @format A list of state fips codes
+#' @format A list of fips codes for 50 states and the District of Columbia
 #' @source \url{https://www.census.gov/geo/reference/ansi_statetables.html}
 #' @examples
 #' fips
