1515ab82a36f1ae43e1311b2a0e6116c1d066a28
Hannah Recht <hrecht@users.noreply.github.com>
2024-04-01 10:59:26 -0400

    build issue

---
+++ R/api-key.R
@@ -0,0 +1,19 @@
+#' Retrieve a Census API key stored the .Renivron file
+#'
+#' @returns A CENSUS_KEY or CENSUS_API_KEY value stored in the user's .Renviron.
+#'   file.
+#' @export
+get_api_key <- function() {
+	if (Sys.getenv("CENSUS_KEY") != "") {
+		key <- Sys.getenv("CENSUS_KEY")
+		key
+	} else if (Sys.getenv("CENSUS_API_KEY") != "") {
+		key <- Sys.getenv("CENSUS_API_KEY")
+		key
+	} else {
+		rlang::warn("You do not have a stored Census API key. Using a key is recommended but not required.\nThe Census Bureau may limit your daily requests.\nRegister for an API key at https://api.census.gov/data/key_signup.html\nand store it in your Renviron file as CENSUS_KEY or CENSUS_API_KEY.\nLearn more at https://www.hrecht.com/censusapi/articles/getting-started.html.",
+								.frequency = "once", .frequency_id = "api_key")
+		key <- NULL
+		key
+	}
+}
